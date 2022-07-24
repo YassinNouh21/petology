c10beb9796261006c5271220a639d16d2842de07
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-07-25 01:50:59 +0200

    style: add all web colors

---
+++ lib/presentation/resources/theme/web_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// on suffex for font
+class WebColor {
+  static const primaryColor = Color(0xFF492F24);
+  static const onPrimaryColor = Color(0xFFFFE3C5);
+  static const secondaryColor = Color(0xFFFFE3C5);
+  static const onSecondaryColor = Color(0xFF492F24);
+  static const tertiaryColor = Color(0xFFA29b98);
+  static const onTertiaryColor = Color(0xFFC9C9C9);
+  static const backgroundColor = Color(0xFFFFFFFF);
+  static const surfaceColor = Color(0xFFEAEAEA);
+  static const onFooterColor = Color(0xFFAE957B);
+}
