e8b3465c7b1f7797438bf6c2e4974fac841af7ec
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-07-25 02:42:18 +0200

    style: add icon paths

---
+++ lib/presentation/resources/theme/web_asset.dart
@@ -0,0 +1,21 @@
+class WebAsset{
+  static const String catIcon = "assets/icons/cat-icon.svg";
+  static const String dogIcon = "assets/icons/dog-icon.svg";
+  static const String emailIcon = "assets/icons/email-icon.svg";
+  static const String locationIcon = "assets/icons/location-icon.svg";
+  static const String notification = "assets/icons/notification-icon.svg";
+  static const String petFootPrintsIcon = "assets/icons/pet-footprints-icon.svg";
+  static const String petologyIcon = "assets/icons/petology-icon.svg";
+  static const String phoneIcon = "assets/icons/phone-icon.svg";
+  static const String solidDogIcon = "assets/icons/solid-dog-icon.svg";
+  static const String solidLocationIcon = "assets/icons/solid-location-icon.svg";
+  static const String solidPhoneIcon = "assets/icons/solid-phone-icon.svg";
+  static const String whatsappIcon = "assets/icons/whatsapp-icon.svg";
+  static const String bowlIcon = "assets/icons/bowl-icon.svg";
+  static const String inoculationIcon = "assets/icons/inoculation-icon.svg";
+  static const String petFoodIcon = "assets/icons/pet-food-icon.svg";
+  static const String sleepingIcon = "assets/icons/sleeping-icon.svg";
+  static const String transportationIcon = "assets/icons/transportation-icon.svg";
+  static const String toyIcon = "assets/icons/toy-icon.svg";
+  static const String vitaminsIcon = "assets/icons/vitamins-icon.svg";
+}
