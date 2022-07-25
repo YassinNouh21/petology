858df30976e109c1685927573894bd582b602757
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-07-25 02:34:14 +0200

    style: add icon paths

---
+++ lib/presentation/resources/theme/web_assets.dart
@@ -0,0 +1,14 @@
+class WebAsset{
+  static const String catIcon = "assets/icons/cat-icon.svg";
+  static const String dogIcon = "assets/icons/dog-icon.svg";
+  static const String emailIcon = "assets/icons/email-icon.svg";
+  static const String locationIcon = "assets/icons/location-icon.svg";
+  static const String notification = "assets/icons/notification-icon.svg";
+  static const String petFootPrintsIcon = "assets/icons/pet-footprints-icon.svg";
+  static const String petologyIcon = "assets/icons/petology-icon.svg";
+  static const String phoneIcon = "assets/icons/phone-icon.svg";
+  static const String solidDogIcon = "assets/icons/solid-dog-icon.svg";
+  static const String solidLocationIcon = "assets/icons/solid-location-icon.svg";
+  static const String solidPhoneIcon = "assets/icons/solid-phone-icon.svg";
+  static const String whatsappIcon = "assets/icons/whatsapp-icon.svg";
+}
