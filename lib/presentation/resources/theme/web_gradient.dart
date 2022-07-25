85c9af673945a55d9f6ccc74aa1bd3cb3de6239d
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-07-25 02:00:41 +0200

    style: add footer and header gradient

---
+++ lib/presentation/resources/theme/web_gradient.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class WebGradient {
+  static const primaryGradient = LinearGradient(
+    colors: [
+      Color(0xFF492F24),
+      Color(0xFF180701),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
